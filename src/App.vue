2c161482f983ac029f647e09646d09523e06cd05
LotusLover <108057253+LotusLover@users.noreply.github.com>
2025-08-20 11:12:52 +0900

    asg1_test

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
-<script setup lang="ts">
-import myComponent from './components/Sample1.vue';
 
+<script setup lang="ts">
+// Sample1.vueコンポーネントをインポートします
+import myComponent from './components/Sample1-asg.vue';
+
+// Vueのリアクティブ変数とライフサイクルフックをインポートします
 import { ref, onMounted } from 'vue';
+
+// 課題表示用のフラグ（trueなら課題名を表示）
 const flgAsg = ref(false);
+// ファイル名を格納する変数
 const fileName = ref('');
+// 課題判定用の文字列（ファイル名に含まれているかチェック）
 const argId = '-asg.vue';
+
+// コンポーネントがマウントされたときに実行される処理
 onMounted(() => {
+  // インポートしたコンポーネントのファイル名を取得
   fileName.value = myComponent.__file.replace(/^.*[\\\/]/, '');
+  // ファイル名に「-asg.vue」が含まれているか判定
   if (-1 < fileName.value.lastIndexOf(argId)) {
+    // 「-asg.vue」を除いた名前に変更
     fileName.value = fileName.value.replace(argId, '');
+    // 課題表示フラグをON
     flgAsg.value = true;
   }
 });
 </script>
 
+
 <template>
+  <!-- flgAsgがtrueのときだけ課題名を表示するブロック -->
   <div v-if="flgAsg" class="question-aire">
     <div class="question">
+      <!-- ファイル名を表示 -->
       <p>{{ fileName }}の課題</p>
     </div>
   </div>
+  <!-- インポートしたコンポーネントを表示 -->
   <myComponent />
 </template>
 
+
 <style scoped>
+/* 課題表示用の枠のスタイル */
 .question-aire {
   padding: 1rem;
   margin-bottom: 1rem;
@@ -33,6 +52,7 @@ onMounted(() => {
     margin: 0;
   }
 }
+/* 課題名部分のスタイル */
 .question {
   padding: 0.5rem;
   border-radius: 10px;
--- src/components/Sample1-asg.vue
+++ src/components/Sample1-asg.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 // 回答数を保持する変数
 const humanities = ref(0);
 const science = ref(0);
+const information = ref(0);
 
 // 回答があった時に実行される関数
 const answer = (field) => {
@@ -11,6 +12,8 @@ const answer = (field) => {
     humanities.value++;
   } else if (field === '理系') {
     science.value++;
+  }else if (field === '情報系') {
+    information.value++;
   }
 };
 
@@ -18,6 +21,8 @@ const answer = (field) => {
 const resetAnswer = () => {
   humanities.value = 0;
   science.value = 0;
+  information.value = 0;
+  console.log('回答がリセットされました');
 };
 </script>
 
@@ -25,9 +30,10 @@ const resetAnswer = () => {
   <p>何系の学部出身ですか？</p>
   <button @click="answer('文系')">文系</button>
   <button @click="answer('理系')">理系</button>
+  <button @click="answer('情報系')">情報系</button>
   <hr />
   <p>回答状況</p>
-  <p>文系：{{ humanities }}　理系：{{ science }}</p>
+  <p>文系：{{ humanities }}　理系：{{ science }}　情報系：{{ information }}</p>
   <button @click="resetAnswer()">回答リセット</button>
 </template>
 
